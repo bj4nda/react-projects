f2b6a7fb34ebcfd923b9aaa92124c46e56866dd8
b4jnda <bharath4jinda@gmail.com>
2023-01-27 16:19:01 +0530

    initial commits

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
+import React, {useState, useEffect} from "react"
+import Loading from "./Loading"
+import Tours from "./Tours"
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project"
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = id => {
+    const newTours = tours.filter(tour => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const getTours = async () => {
+    setLoading(true)
+
+    try {
+      const res = await fetch(url)
+      const tours = await res.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div className="title">
+        <h2>no Tours left</h2>
+        <button className="btn" onClick={() => getTours()}>
+          {" "}
+          refresh{" "}
+        </button>
+      </div>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
